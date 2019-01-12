c513c82106741748dd336dbf9440067082a6d296
MariusValentinDirvareanu <dirvareanumarius@gmail.com>
2019-01-12 21:05:49 +0200

    Liste Duble. Trebuie refacuta stergerea de element + stergerea de element de la lista simpla

---
+++ ListeDuble/ListeDuble/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "ListaDubla.h"
+
+using namespace std;
+
+int main() // program liste
+{
+	ListaDubla listadubla(3);
+	listadubla.parcurgere_lista();
+	listadubla.parcurgere_lista_invers();
+	listadubla.adaugaElementInterior(20, 2);
+	listadubla.parcurgere_lista();
+	listadubla.adaugareLaInceput(1000);
+	listadubla.adaugareLaSfarsit(2000);
+	listadubla.parcurgere_lista();
+	listadubla.parcurgere_lista_invers();
+	listadubla.stergereElementLista(2);
+	listadubla.parcurgere_lista();
+	listadubla.parcurgere_lista_invers();
+	listadubla.stergere_lista();
+	listadubla.parcurgere_lista();
+	listadubla.parcurgere_lista_invers();
+	system("PAUSE");
+	return 0;
+}
